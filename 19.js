66396451dc9ff38b7a28179c465565958b114398
Abdul Rahman <Abdulrahman.81869@gmail.com>
2024-11-08 01:52:08 +0000

    Practice Code

--- 19.js
+++ 19.js
@@ -1,1 +1,185 @@
 // break, switch and continue:
+
+// break:
+// the break statement is used to
+// terminate the loop immdeiately
+// when its executed
+
+let table = 5;
+let startfrom = 1;
+let endat = 15;
+let stopat = 15;
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//     console.log(`${table} X ${startfrom} = ${table * startfrom}`);
+//     if(startfrom == stopat){
+//         break;
+//     }
+// }
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//   console.log(`${table*startfrom}`);
+//    if(startfrom==stopat){
+//     break;
+//    }
+// }
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//     console.log(`${table*startfrom}`);
+//     if(startfrom==stopat){
+//         break;
+//     }
+// }
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//     console.log(`${table*startfrom}`);
+//     if(startfrom==stopat){
+//         break;
+//     }
+// }
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//     console.log(`${table*startfrom}`);
+//     if(startfrom == stopat){
+//         break;
+//     }
+// }
+
+// continue: The continue statement skips the current iteration of the loop and proceeds to the next iteration.
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//     if(startfrom==stopat){
+//         continue;
+//      }
+//     console.log(`${table*startfrom}`);
+
+// }
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//     if(startfrom==stopat){
+//         continue;
+//     }
+//     console.log(`${table * startfrom}`);
+
+// }
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//     if(startfrom>=10 && startfrom<=13){
+//         continue;
+//     }
+//     console.log(`${table*startfrom}`);
+
+// }
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//     if(startfrom>=5 && startfrom<=10){
+//         continue;
+//     }
+//     console.log(`${table*startfrom}`);
+
+// }
+
+// for(startfrom; startfrom<=endat; startfrom++){
+//     if(startfrom>=1 && startfrom<=10){
+//         continue;
+//     }
+//     console.log(`${table*startfrom}`);
+
+// }
+
+// Switch statement: The JavaScript switch...case statement executes different blocks of code based on the value of a given expression.
+
+// write a program to check if the given fruit is matching or not.
+
+// let fruit = "mango";
+
+// switch (fruit) {
+//   case "apple":
+//     console.log("You have apple");
+//     break;
+//   case "Banana":
+//     console.log("You have banana");
+//     break;
+//   case "mango":
+//     console.log("You have a Mango");
+//     break;
+//   default:
+//     console.log("you don't have any fruit");
+// }
+
+// let fruit = "grapes";
+
+// switch (fruit) {
+//   case "apple":
+//     console.log("you have an apple");
+//     break;
+//   case "strawberry":
+//     console.log("you have a berry");
+//     break;
+//   case "grapes":
+//     console.log("you have grapes");
+//     break;
+//   default:
+//     console.log("you don't have any fruit");
+// }
+
+
+
+// let weekday = 0;
+
+// switch (weekday) {
+//   case 0:
+//     console.log("Sunday");
+//     break;
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   default:
+//     console.log("invalid weekday number");
+// }
+
+
+
+
+// let weekday = 0;
+
+// switch (weekday) {
+//   case 0:
+//     console.log("Sunday");
+//     break;
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   default:
+//     console.log("invalid weekday number");
+// }
