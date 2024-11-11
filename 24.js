d32708e6b9ebccc319aa328a499715d09d2960fd
Abdul Rahman <Abdulrahman.81869@gmail.com>
2024-11-11 02:01:23 +0000

    Practice code

---
+++ 24.js
@@ -0,0 +1,27 @@
+et flipkartUser = {
+    name: "abdul rahman",
+    PhoneNo: 8186978069,
+    userId: "2219A7E",
+    userDetails: {
+        accountCreated: 29_01_2024,
+        userPurchases: 3 + " items",
+        drafts: 4,
+        userAddress: {
+            country: "India",
+            city: "Hyderabad",
+            street: "Chandulal Baradari, near Bahadurpura" 
+        }
+    }
+}
+
+flipkartUser.name = "Syed Abdul Rahman";
+flipkartUser.PhoneNo = 8686813770;
+// console.log(flipkartUser);
+
+flipkartUser.userDetails.accountCreated = 12_11_2013;
+flipkartUser.userDetails.userPurchases = 6 + " items";
+flipkartUser.userDetails.drafts = 8;
+// console.log(flipkartUser.userDetails);
+
+// delete flipkartUser.userDetails.userAddress;
+// console.log(flipkartUser);
