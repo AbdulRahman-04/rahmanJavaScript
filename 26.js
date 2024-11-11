054a1aa73b1b2e7cc7dde3e1d981599c5ae117e5
Abdul Rahman <Abdulrahman.81869@gmail.com>
2024-11-11 02:03:34 +0000

    Practice code

---
+++ 26.js
@@ -0,0 +1,37 @@
+// Arrays: it is a collection of elements or items.
+
+// array starts from 0th index
+
+let fruits = ["apple", "mango", "banana", "orange"]
+// index         0        1        2          3
+
+// console.log(fruits);
+
+let myArray = ["hello", 25, true, false, null, undefined, "yoo", 5049, {name: "rahman", rollNo: 49}, [12,32,45]];
+
+
+// reading values in arrays:
+
+// console.log(myArray[0], myArray[1], myArray[2], myArray[3]);
+
+// console.log(myArray[4], myArray[5], myArray[6]);
+
+// console.log(myArray[8]);
+
+// console.log(myArray[9][2]);
+
+
+// updating values in arrays
+// myArray[1] = 30;
+// myArray[4] = -"12";
+// console.log(myArray);
+
+delete myArray[3], delete myArray[4], delete myArray[5], delete myArray[6];
+console.log(myArray);
+
+
+// myArray[3] = "3rd index value";
+// myArray[4] = "4th index value";
+// myArray[5] = "5th index value";
+// myArray[6] = "6th index value";
+// console.log(myArray);
